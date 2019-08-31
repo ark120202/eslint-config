828b5572f71582ba96e5805db7b03801e8adefe4
ark120202 <ark120202@gmail.com>
2019-09-01 03:54:48 +0500

    refactor(typescript): don't extend `all` config

--- bases/typescript.js
+++ bases/typescript.js
@@ -3,7 +3,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
-    'plugin:@typescript-eslint/all',
+    'plugin:@typescript-eslint/base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:import/typescript',
     'prettier/@typescript-eslint',
@@ -12,6 +12,15 @@ module.exports = {
     warnOnUnsupportedTypeScriptVersion: false,
   },
   rules: {
+    // Covered by TypeScript
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'import/no-unresolved': 'off',
+    'import/named': 'off',
+    'import/default': 'off',
+    'import/namespace': 'off',
+    'import/export': 'off',
+
     // `allowRequire` is covered by @typescript-eslint/no-require-imports
     'import/no-commonjs': ['error', { allowRequire: true }],
     // Covered by @typescript-eslint/no-require-imports
@@ -22,14 +31,9 @@ module.exports = {
     // TODO: Should be covered by a type-based rule
     // 'import/no-deprecated': 'off',
 
-    // Covered by TypeScript
-    'import/no-unresolved': 'off',
-    'import/named': 'off',
-    'import/default': 'off',
-    'import/namespace': 'off',
-    'import/export': 'off',
-
-    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/adjacent-overload-signatures': 'error',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/await-thenable': 'error',
     '@typescript-eslint/ban-types': [
       'error',
       {
@@ -40,50 +44,60 @@ module.exports = {
         },
       },
     ],
+    camelcase: 'off',
     '@typescript-eslint/camelcase': ['error', { properties: 'never', ignoreDestructuring: true }],
+    '@typescript-eslint/class-name-casing': 'error',
     // TODO: '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/consistent-type-definitions': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': [
       'error',
       { overrides: { constructors: 'no-public' } },
     ],
     '@typescript-eslint/generic-type-naming': ['error', '^T([A-Z][A-Za-z]*)?$'],
+    '@typescript-eslint/interface-name-prefix': 'error',
     // TODO: Enable?
     // '@typescript-eslint/member-naming': 'error',
-    '@typescript-eslint/member-ordering': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'error',
+    'no-array-constructor': 'off',
+    '@typescript-eslint/no-array-constructor': 'error',
+    '@typescript-eslint/no-empty-interface': 'error',
+    '@typescript-eslint/no-extraneous-class': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-for-in-array': 'error',
     '@typescript-eslint/no-inferrable-types': [
       'error',
+      // TODO: 2.0.0 - remove
       { ignoreParameters: false, ignoreProperties: false },
     ],
-    '@typescript-eslint/no-magic-numbers': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    // https://github.com/typescript-eslint/typescript-eslint/issues/103
-    '@typescript-eslint/no-parameter-properties': 'off',
-    '@typescript-eslint/no-this-alias': ['error', { allowDestructuring: true }],
-    '@typescript-eslint/no-triple-slash-reference': 'off',
-    // TODO: Conflicts with @typescript-eslint/consistent-type-definitions
-    '@typescript-eslint/no-type-alias': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    // Covered by @typescript-eslint/no-require-imports
-    '@typescript-eslint/no-var-requires': 'off',
-    // Covered by unicorn/no-for-loop
-    // TODO: prefer after https://github.com/typescript-eslint/typescript-eslint/issues/702
-    '@typescript-eslint/prefer-for-of': 'off',
-    // TODO: Covered by @typescript-eslint/consistent-type-definitions
+    '@typescript-eslint/no-misused-new': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-namespace': 'error',
+    '@typescript-eslint/no-object-literal-type-assertion': 'error',
+    '@typescript-eslint/no-require-imports': 'error',
+    '@typescript-eslint/no-this-alias': [
+      'error',
+      // TODO: 2.0.0 - remove
+      { allowDestructuring: true },
+    ],
+    '@typescript-eslint/no-unnecessary-qualifier': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    // TODO: Enable instead of unicorn/no-for-loop after https://github.com/typescript-eslint/typescript-eslint/issues/702
+    // '@typescript-eslint/prefer-for-of': 'off',
     '@typescript-eslint/prefer-function-type': 'error',
-    '@typescript-eslint/prefer-regexp-exec': 'off',
+    '@typescript-eslint/prefer-includes': 'error',
+    '@typescript-eslint/prefer-namespace-keyword': 'error',
+    '@typescript-eslint/prefer-readonly': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
     '@typescript-eslint/promise-function-async': [
       'error',
-      // Works differently from tslint
       { allowAny: true, checkArrowFunctions: false },
     ],
-    '@typescript-eslint/unbound-method': 'off',
-
-    // https://github.com/typescript-eslint/typescript-eslint/issues/698, https://github.com/typescript-eslint/typescript-eslint/pull/691
-    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/require-array-sort-compare': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    '@typescript-eslint/triple-slash-reference': 'error',
+    '@typescript-eslint/unified-signatures': 'error',
   },
 };
