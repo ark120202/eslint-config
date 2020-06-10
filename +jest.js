d042d569f7669a5cef743ba9012238ff477be3c0
ark120202 <ark120202@gmail.com>
2020-06-10 09:15:53 +0500

    chore: upgrade `eslint-plugin-jest`

--- +jest.js
+++ +jest.js
@@ -5,11 +5,17 @@ module.exports = {
     'jest/expect-expect': 'off',
     'jest/consistent-test-it': ['error', { fn: 'test', withinDescribe: 'test' }],
     'jest/lowercase-name': 'error',
-    'jest/no-duplicate-hooks': 'error',
-    'jest/no-empty-title': 'error',
-    'jest/no-expect-resolves': 'error',
+    // TODO: Should be covered by a type-based rule
+    'jest/no-deprecated-functions': 'error',
+    'jest/no-restricted-matchers': [
+      'error',
+      {
+        resolves: 'Use `expect(await promise)` instead.',
+        toBeTruthy: 'Avoid `toBeTruthy`',
+        toBeFalsy: 'Avoid `toBeFalsy`',
+      },
+    ],
     'jest/no-test-return-statement': 'error',
-    'jest/no-truthy-falsy': 'error',
     'jest/prefer-spy-on': 'error',
     'jest/prefer-todo': 'error',
     'jest/valid-title': 'error',
